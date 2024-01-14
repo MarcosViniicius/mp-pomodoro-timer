34fe31238f2d3249aea9adee91e602102f48544c
Marcos Vinicius de lucena <marcosvinicius.lucena27@gmail.com>
2024-01-14 19:23:13 -0300

    função de lista

--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,1 +1,32 @@
-// Faça a lógica referente as tarefas aqui!
+var tasks = [];
+var listTasks = document.getElementById("tasks");
+
+function addTask() {
+  var newTask = document.getElementById("new-task").value;
+  tasks.push(newTask);
+  // Limpa a lista antes de adicionar as tarefas para evitar duplicatas
+  listTasks.innerHTML = "";
+  for (var i = 0; i < tasks.length; i++) {
+    listTasks.innerHTML +=
+      "<ul>" +
+      tasks[i] +
+      " <button onclick='removeTask(" +
+      i +
+      ")'>X</button></ul>";
+  }
+}
+
+function removeTask(index) {
+  tasks.splice(index, 1); // Remove a tarefa do array
+  var listTasks = document.getElementById("tasks");
+  // Atualiza a lista após remover a tarefa
+  listTasks.innerHTML = "";
+  for (var i = 0; i < tasks.length; i++) {
+    listTasks.innerHTML +=
+      "<ul>" +
+      tasks[i] +
+      " <button onclick='removeTask(" +
+      i +
+      ")'>X</button></ul>";
+  }
+}
